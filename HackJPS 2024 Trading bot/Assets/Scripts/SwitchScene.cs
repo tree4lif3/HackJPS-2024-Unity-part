354b6b5e3ca75c1e4051745e13715e2237b6e8e3
NotMarkerDev <marcus.ngchun@gmail.com>
2024-06-09 14:27:08 -0400

    Control Menu

--- HackJPS 2024 Trading bot/Assets/Scripts/SwitchScene.cs
+++ HackJPS 2024 Trading bot/Assets/Scripts/SwitchScene.cs
@@ -5,22 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
+    public Animator animator;
     public GameObject mainMenu;
     public GameObject controls;
 
     private void Start()
     {
         controls.SetActive(false);
+        mainMenu.SetActive(true);
+
+        animator.SetBool("Back Button", false);
+        animator.SetBool("ControlButton", false);
     }
 
     // Start is called before the first frame update
     public void Controls()
     {
-        SceneManager.LoadScene("Controls");
+        mainMenu.SetActive(false);
+
+        animator.SetBool("ControlButton", true);
+        animator.SetBool("Back Button", false);
+        controls.SetActive(true);
     }
     public void Back()
     {
-        SceneManager.LoadScene("Main Menu");
+        controls.SetActive(false);
+        mainMenu.SetActive(true);
+
+        animator.SetBool("Back Button", true);
+        animator.SetBool("ControlButton", false);
     }
     public void BusinessGame()
     {
